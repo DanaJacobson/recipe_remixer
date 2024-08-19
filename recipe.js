a50208875005b40c5e7e2cf835ac96b8f93a4672
Yuval Yehie <yuvalyehie@gmail.com>
2024-08-19 16:04:56 +0300

    Adding a comment option to the recipe after saving it.

--- recipe.js
+++ recipe.js
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', () => {
       container.appendChild(ingredientsTitle);
 
       const ingredients = modifiedRecipe['List of ingredients'];
-      if (ingredients) {
+      if (Array.isArray(ingredients)) {
           const ingredientsList = document.createElement('ul');
           ingredients.forEach(ingredient => {
               const li = document.createElement('li');
